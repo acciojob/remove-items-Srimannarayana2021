6db718281d448b1f66a32b2704491cb5e30ac65a
Srimannarayana <87487895+Srimannarayana2021@users.noreply.github.com>
2025-09-02 09:32:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+const button = document.querySelector('input[type="button"]');
+    const select = document.getElementById("colorSelect");
+
+    button.addEventListener("click", function() {
+      if (select.selectedIndex !== -1) {  // ensure something is selected
+        select.remove(select.selectedIndex);
+      }
+    });
